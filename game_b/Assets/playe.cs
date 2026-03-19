c98ad14a727086dd92ba1a290db5b522c06c2625
comet0919 <shs070919@gmail.com>
2026-03-19 15:30:40 +0900

    플레이어 기본 움직임

--- game_b/Assets/playe.cs
+++ game_b/Assets/playe.cs
@@ -5,6 +5,26 @@ public class playe : MonoBehaviour
 {
     private Vector2 movelnput;
     public float moveSpssd = 7f;
+    public float JumpForce = 7f;
+    private Rigidbody2D rd;
+    private Animator myAnimator;
+
+    void Start()
+    {
+        rd = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
+
+    public void OnMowe(InputValue value)
+    {
+        movelnput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        rd.linearVelocity = new Vector2(rd.linearVelocity.x, JumpForce);
+    }
     public void OnMove(InputValue value)
     {
         movelnput = value.Get<Vector2>();
@@ -18,6 +38,16 @@ public class playe : MonoBehaviour
         else if(movelnput.x < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+        }
+        transform.Translate(Vector3.right * moveSpssd * movelnput.x * Time.deltaTime);
+
+        if(movelnput.magnitude > 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
         }
         transform.Translate(Vector3.right * moveSpssd * movelnput.x * Time.deltaTime);
     }
